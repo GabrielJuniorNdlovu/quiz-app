a35274c8d3c00366ce8183f7e37fe421323209d7
Theeangelgabriel_ <ndlovujnr18@gmail.com>
2025-02-16 10:41:10 +0200

    started a quiz app project removed pre-existing vite code add components folder add Quiz.jsx components add Quiz.css updated .gitignore to not commit node_modules add code to created components

---
+++ src/Components/Quiz/Quiz.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Quiz.css'
+
+const Quiz = () => {
+  return (
+    <div>
+      <div className="container">
+        <h1>Quiz App</h1>
+        <hr/>
+        <h2>Which device is required for the Internet connection</h2>
+        <ul>
+            <li>Moderm</li>
+            <li>Router</li>
+            <li>LAN Cable</li>
+            <li>Pen Drive</li>
+        </ul>
+        <button>Next</button>
+        <div className="index">
+            1 of 5 questions
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Quiz
